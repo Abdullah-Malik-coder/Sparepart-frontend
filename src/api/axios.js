e09bafa8aa11c029d0c58ee03773f320a4b9c95a
Abdullah <am7071363@gmail.com>
2025-08-05 17:12:21 +0500

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -12,7 +12,6 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL: import.meta.env.REACT_APP_API_BASE_URL,
-  withCredentials: true, // if you're using cookies/session
 });
 
 export default instance;
